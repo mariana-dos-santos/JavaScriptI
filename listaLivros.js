32bf5c9dea30aec227dcf3d0249085d80390dbc6
mariana-dos-santos <santos.moreira.mariana@escola.pr.gov.br>
2026-04-09 14:12:24 +0000

    Trabalho 1 Finalizado

---
+++ listaLivros.js
@@ -0,0 +1,28 @@
+const precosLivros = [
+    {
+        titulo: "São Mateus",
+        preco: 30
+    },
+    {
+        titulo: "São Lucas",
+        preco: 60
+    },
+    {
+        titulo: "São João",
+        preco: 80
+    },
+    {
+        titulo: "São Marcos",
+        preco: 35
+    },
+    {
+        titulo: "Primeira Carta de São João",
+        preco: 40
+    },
+    {
+        titulo: "Primeira Carta de São Paulo aos Filipenses",
+        preco: 50
+    }
+];
+
+module.exports = precosLivros;
